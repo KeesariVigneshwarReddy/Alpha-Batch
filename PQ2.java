949815a96e0e4da70b9fbfcfe7cd370981b897b7
KeesariVigneshwarReddy <122781779+KeesariVigneshwarReddy@users.noreply.github.com>
2023-05-10 09:25:51 +0530

    Practice Question 2

---
+++ PQ2.java
@@ -0,0 +1,13 @@
+/* 2) Input side of a square and print area */
+import java.util.*;
+public class PQ2
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        // input side
+        int s = sc.nextInt();
+        //print area
+        System.out.println(s * s);
+    }
+}
