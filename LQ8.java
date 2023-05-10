22d09cc515f17226c51daa84df8f60747971d918
KeesariVigneshwarReddy <122781779+KeesariVigneshwarReddy@users.noreply.github.com>
2023-05-10 15:40:55 +0530

    Lecture Question 8
    Lecture Question 8

---
+++ LQ8.java
@@ -0,0 +1,15 @@
+/* 8) Print HW using do while loop */
+import java.util.*;
+public class LQ8
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+        do 
+        {
+            System.out.println("HW");
+            i++;
+        } while (i < 10);
+    }
+}
