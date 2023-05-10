1e4f69ab285289eca565a9828187094a4c0988f3
KeesariVigneshwarReddy <122781779+KeesariVigneshwarReddy@users.noreply.github.com>
2023-05-10 15:50:30 +0530

    Practice Question 4
    Practice Question 4

---
+++ PQ4.java
@@ -0,0 +1,15 @@
+/* 4) Write a program to print table of any number */
+import java.util.*;
+public class PQ4
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println(n + " * " + i + " = " + (n * i));
+        }
+    }
+}
