321d281ecdc35dc5a47d43fc38afb0d4bdcbc037
KeesariVigneshwarReddy <122781779+KeesariVigneshwarReddy@users.noreply.github.com>
2023-05-10 09:15:15 +0530

    Lecture Question 7

---
+++ LQ7.java
@@ -0,0 +1,16 @@
+/* 7) Product of a and b input from user */
+import java.util.*;
+public class LQ7
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        // input a
+        int a = sc.nextInt();
+        // input b
+        int b = sc.nextInt();
+        System.out.print("Product = ");
+        // print product
+        System.out.print(a * b);
+    }
+}
