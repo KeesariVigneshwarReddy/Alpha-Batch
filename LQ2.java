66750da9b580e13a0faf4a249107d53c36f29081
KeesariVigneshwarReddy <122781779+KeesariVigneshwarReddy@users.noreply.github.com>
2023-05-10 15:33:36 +0530

    Lecture Question 2
    Lecture Question 2

---
+++ LQ2.java
@@ -0,0 +1,15 @@
+/* 2) Print number from 1 to 10 using while loop */
+import java.util.*;
+public class LQ2
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int i = 1;
+        while (i <= 10)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
