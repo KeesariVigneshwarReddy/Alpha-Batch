fa8ae16c59cda1e9a8b92b03e0bd03ec02a595be
KeesariVigneshwarReddy <122781779+KeesariVigneshwarReddy@users.noreply.github.com>
2023-05-10 09:10:37 +0530

    Create LQ2.java

---
+++ LQ2.java
@@ -0,0 +1,17 @@
+/* 2) Print Hello World on next line */
+public class Main
+{
+    public static void main(String args[]) 
+    {
+        System.out.print("Hello World\nHello World");
+    }
+}
+                        (or)
+public class Main
+{
+    public static void main(String args[]) 
+    {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
