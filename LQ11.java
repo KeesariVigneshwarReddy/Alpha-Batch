eae90568fa5baf6400996468b86c0205f72716a1
KeesariVigneshwarReddy <122781779+KeesariVigneshwarReddy@users.noreply.github.com>
2023-05-10 09:20:20 +0530

    Lecture Question 11

---
+++ LQ11.java
@@ -0,0 +1,14 @@
+/* 11) Type Casting
+       character to integer
+ */
+import java.util.*;
+public class LQ11
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        char a = 'f';
+        int b = (int)a;
+        System.out.println(b);;
+    }
+}
