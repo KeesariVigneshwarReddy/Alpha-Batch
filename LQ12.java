06b6690689ac0a8ffacf866ff23d1d2d5c8d0e5a
KeesariVigneshwarReddy <122781779+KeesariVigneshwarReddy@users.noreply.github.com>
2023-05-10 09:22:31 +0530

    Lecture Question 12

---
+++ LQ12.java
@@ -0,0 +1,13 @@
+/* 12) Type promotion in java*/
+import java.util.*;
+public class LQ12
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        short a = 5;
+        byte b = 25;
+        char c = 'c';
+        System.out.println(a + b + c);
+    }
+}
