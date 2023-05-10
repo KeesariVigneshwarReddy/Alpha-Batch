9da5f8a05f9eb72091ebe8de82dd68286dab6808
KeesariVigneshwarReddy <122781779+KeesariVigneshwarReddy@users.noreply.github.com>
2023-05-10 09:13:34 +0530

    Lecture Question 5

---
+++ LQ5.java
@@ -0,0 +1,11 @@
+/* 5) Sum of 10 and 5 */
+public class LQ5
+{
+    public static void main(String args[]) 
+    {
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
