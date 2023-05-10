101285de2578eef66505a452c036b8980e1348e5
KeesariVigneshwarReddy <122781779+KeesariVigneshwarReddy@users.noreply.github.com>
2023-05-10 15:37:51 +0530

    Lecture Question 5
    Lecture Question 5

---
+++ LQ5.java
@@ -0,0 +1,13 @@
+/* 5) Print HW 100 times using for loop */
+import java.util.*;
+public class LQ5
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 1; i <= 100; i++)
+        {
+            System.out.println("HW");
+        }        
+    }
+}
