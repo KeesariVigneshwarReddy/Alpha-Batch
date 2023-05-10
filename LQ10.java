d7386dc6d37ab9b39fd06ad9cb61bfba9f1df19e
KeesariVigneshwarReddy <122781779+KeesariVigneshwarReddy@users.noreply.github.com>
2023-05-10 09:19:08 +0530

    Lecture Question 10

---
+++ LQ10.java
@@ -0,0 +1,12 @@
+/* 10) Type Casting */
+import java.util.*;
+public class LQ10
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        float a = 2.36f;
+        int b = (int)a;
+        System.out.println(b);;
+    }
+}
