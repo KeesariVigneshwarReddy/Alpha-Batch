0b0acfbb2467a283f78928d64534db5d25bab2e7
KeesariVigneshwarReddy <122781779+KeesariVigneshwarReddy@users.noreply.github.com>
2023-05-10 09:23:46 +0530

    Lecture Question 13

---
+++ LQ13.java
@@ -0,0 +1,14 @@
+/* 13) Type promotion in java*/
+import java.util.*;
+public class LQ13
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = 10;
+        float b = 20.25f;
+        long c = 25;
+        double d = 30;
+        System.out.println(a + b + c + d);
+    }
+}
