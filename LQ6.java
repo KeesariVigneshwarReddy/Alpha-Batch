9b714fcf3e18e2abd02fc4c7282651120c13bb7d
KeesariVigneshwarReddy <122781779+KeesariVigneshwarReddy@users.noreply.github.com>
2023-05-10 15:38:55 +0530

    Lecture Question 6
    Lecture Question 6

---
+++ LQ6.java
@@ -0,0 +1,20 @@
+/* 6) Print Square pattern
+    * * * *
+    * * * *
+    * * * *
+    * * * *
+ */
+import java.util.*;
+public class LQ6
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        // repeat 4 times in row
+        for (int i = 1; i <= 4; i++)
+        {
+            // for one line
+            System.out.println("* * * *\n");
+        }        
+    }
+}
