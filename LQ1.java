35a3351b7f2e2c7faab941a48162b03782c0c17c
KeesariVigneshwarReddy <122781779+KeesariVigneshwarReddy@users.noreply.github.com>
2023-05-12 14:23:29 +0530

    Lecture Question 1
    Lecture Question 1

---
+++ LQ1.java
@@ -0,0 +1,15 @@
+/* 1) Print HW using function */
+import java.util.*;
+public class LQ1
+{
+    // function is not returning a value so it's return type is void
+    public static void printHW()
+    {
+        System.out.println("HW");
+    }
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        printHW();
+    }
+}
