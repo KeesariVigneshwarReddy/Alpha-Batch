bef2194d08107ac658766839a678d2fc543e90b5
KeesariVigneshwarReddy <122781779+KeesariVigneshwarReddy@users.noreply.github.com>
2023-05-12 14:39:10 +0530

    Lecture Question 1
    Lecture Question 1

---
+++ LQ1.java
@@ -0,0 +1,31 @@
+/* 1) Pattern
+    ****
+    *  *
+    *  *
+    ****
+ */
+import java.util.*;
+public class LQ1
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (i == 1 || j == 1 || i == n || j == m)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+}
