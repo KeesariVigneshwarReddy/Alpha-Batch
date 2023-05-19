06b4d225f356a6aed129033377a24051967fb8fb
KeesariVigneshwarReddy <122781779+KeesariVigneshwarReddy@users.noreply.github.com>
2023-05-19 14:46:38 +0530

    Lecture Question 12
    Lecture Question 12

---
+++ LQ12.java
@@ -0,0 +1,29 @@
+/* 12) Remove all duplicates in a string    */
+import java.util.*;
+public class LQ12
+{
+    public static void removeDuplicates(String str, int idx, StringBuilder newStr, boolean map[])
+    {
+        if (idx == str.length())
+        {
+            System.out.println(newStr);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if (map[currChar - 'a'] == true)
+        {
+            removeDuplicates(str, idx + 1, newStr, map);
+        }
+        else
+        {
+            map[currChar - 'a'] = true;
+            removeDuplicates(str, idx + 1, newStr.append(currChar), map);
+        }
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner (System.in);
+        String str = sc.next();
+        removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
+    }
+}
