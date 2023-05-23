01ce71f15528368abf9be456759d0c6ce63fc9e5
KeesariVigneshwarReddy <122781779+KeesariVigneshwarReddy@users.noreply.github.com>
2023-05-23 13:17:43 +0530

    Create LQ2.java

---
+++ LQ2.java
@@ -0,0 +1,11 @@
+/* 2) Print length of an array */  
+import java.util.*;
+public class LQ2
+{
+    public static void main(String[] args)
+    {
+        int marks[] = new int[10];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Length = " + marks.length);
+    }
+}
