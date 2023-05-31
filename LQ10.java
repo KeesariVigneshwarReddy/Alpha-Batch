ee6d6523b97359f22c6561f4cd2453fa238dbfa4
KeesariVigneshwarReddy <122781779+KeesariVigneshwarReddy@users.noreply.github.com>
2023-05-31 14:25:40 +0530

    Create LQ10.java

---
+++ LQ10.java
@@ -0,0 +1,51 @@
+/* 10) Pair sum 1
+       Find if any pair in a Sorted ArrayList has a target sum
+       (Brute Force) */
+import java.util.ArrayList;
+import java.util.*;
+import java.util.Collections;
+public class LQ10
+{
+    // this function returns true if target sum is possible
+    public static boolean findTargetSum(ArrayList<Integer> list, int target)
+    {
+        // making all possible pairs
+        for (int i = 0; i < list.size(); i++)
+        {
+            for (int j = 0; j < list.size(); j++)
+            {
+                // adding the elements of all possible pairs and comparing with target
+                if (list.get(i) + list.get(j) == target)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        // creating array list
+        ArrayList<Integer> list = new ArrayList<>();
+        // inputing number of elements
+        int n = sc.nextInt();
+        // inputing elements in AL
+        for (int i = 0 ; i < n; i++)
+        {
+            int temp = sc.nextInt();
+            list.add(i, temp);
+        }
+        // initializing the target sum
+        int target = sc.nextInt();
+        // this function returns true if target sum is possible
+        if (findTargetSum(list, target))
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
