34edc9f744f829b9f7b119fc3c43d1b621cc8591
KeesariVigneshwarReddy <122781779+KeesariVigneshwarReddy@users.noreply.github.com>
2023-05-31 14:18:37 +0530

    Create LQ1.java

---
+++ LQ1.java
@@ -0,0 +1,10 @@
+/* 1) Create an arraylist of Integer class */
+import java.util.ArrayList;
+import java.util.*;
+public class LQ1
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+    }
+}
