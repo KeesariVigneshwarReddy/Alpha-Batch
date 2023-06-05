23633706a4f53123478ff0d31f12c7fb1b67e7b8
KeesariVigneshwarReddy <122781779+KeesariVigneshwarReddy@users.noreply.github.com>
2023-06-05 16:24:26 +0530

    Create LQ8.java

---
+++ LQ8.java
@@ -0,0 +1,75 @@
+/* 8) Mirror a BST */
+import java.util.*;
+public class LQ8
+{
+    // creating a node
+    static class Node
+    {
+        // properties of node
+        int data;
+        Node left;
+        Node right;
+
+        // constructor
+        Node(int data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static void preOrder(Node root)
+    {
+        if (root == null)
+        {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+    public static Node mirror(Node root)
+    {
+        // base case
+        if (root == null)
+        {
+            return null;
+        }
+        // recursive call left sub tree mirror
+        Node leftMirror = mirror(root.left);
+        // recursive call right sub tree mirror
+        Node rightMirror = mirror(root.right);
+        // let root.left point to right mirror
+        root.left = rightMirror;
+        // let root.right point to left mirror
+        root.right = leftMirror;
+        return root;
+    }
+    public static void main(String[] args)
+    {
+        /*
+                    8
+                   / \
+                  5   10
+                 / \    \
+                3   6    11
+        */
+        Node root = new Node(8);
+        root.left = new Node(5);
+        root.right = new Node(10);
+        root.left.left = new Node(3);
+        root.left.right = new Node(6);
+        root.right.right = new Node(11);
+        preOrder(root);
+        System.out.println();
+        /*
+                    8
+                   / \
+                  10  5
+                 /   / \
+                11  6   3
+        */
+        root = mirror(root);
+        preOrder(root);
+    }
+}
